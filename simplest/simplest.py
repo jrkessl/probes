9e4b8564d37f6e0e887ca36d98e3b7b303d27d2c
Juliano Kessler <juliano.kessler@ticketmaster.com>
2025-01-25 12:26:36 -0300

    trying to improve simplest and its watcher

--- simplest/simplest.py
+++ simplest/simplest.py
@@ -9,12 +9,21 @@ if __name__ == "__main__":
 
 counter = 0 
 
-# This is the code that runs when you call the prefix "/business"
-@app.route('/business', methods=['GET'])
-def business():
+# This is the code that runs when you call the prefix "/safe"
+@app.route('/safe', methods=['GET'])
+def safe():
+    return "Safe request completed", 200 # It does not do anything. Just returns immediately. But it crashes on every 10th request. 
+
+
+@app.route('/crash_tenth', methods=['GET'])
+def crash_tenth():
     global counter
     counter = counter + 1 
     if counter == 10:
         os._exit(1)
     else:
-        return "business request completed", 200 # It does not do anything. Just returns immediately. But it crashes on every 10th request. 
+        return f"Crashing in {counter}", 200 # It returns OK most of the time; there is a random risk of crashing. 
+
+@app.route('/error', methods=['GET'])
+def error():
+    return "Here is your error!", 500 # Always returns an error. But the server process is unaffected. 
